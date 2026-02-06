ec7a502998e55ca2c87a595972b7418f1fbc7aed
Martiniano <martibidini@gmail.com>
2026-02-05 23:28:13 -0300

    Creacion de el Array

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+let Total;
+
+const Perfumes=[
+    {Nombre:"Yara Pink",Categoria:"Mujer",Precio:82.000},
+    {Nombre:"Mandarin Sky",Categoria:"Hombre",Precio:85.000},
+    {Nombre:"Fakhar Black",Categoria:"Hombre",Precio:60.000},
+    {Nombre:"Yum Yum",Categoria:"Mujer",Precio:80.000},
+    {Nombre:"9 PM",Categoria:"Hombre",Precio:90.000},
+    {Nombre:"Yara Candy",Categoria:"Mujer",Precio:0.000},
+    {Nombre:"Club de Nuit Intense",Categoria:"Hombre",Precio:95.000},
+    {Nombre:"Hawas Ice",Categoria:"Hombre",Precio:70.000},
+    {Nombre:"Honor & Glory",Categoria:"Mujer",Precio:80.000},
+    {Nombre:"The Kingdom For Men",Categoria:"Hombre",Precio:90.000},
+]
+
+
+
+
+
