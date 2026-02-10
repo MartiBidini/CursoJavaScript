66b5a099eadeda4987dbe4c608f43cc303e27973
Martiniano <martibidini@gmail.com>
2026-02-09 23:04:08 -0300

    Mejora en texto de tranferencia v2

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ let IniciarPrograma=prompt("Bienvenido a Olfate-e Perfumeria,Si desea comprar el
     
         if(TotalCompra > 0 ){
     
-        let ElegirPago=parseInt(prompt("Si desea pagar con tarjeta eliga 1 si es con tranferencia(15% de descuento) eliga 2"));
+        let ElegirPago=parseInt(prompt("Si desea pagar con tarjeta eliga 1, si desea pagar con tranferencia(15% de descuento) eliga 2"));
         while(ElegirPago!=1 && ElegirPago !=2 || isNaN(ElegirPago)){
             alert("Opcion incorrecta");
             ElegirPago=parseInt(prompt("Si desea pagar con tarjeta eliga 1, si desea pagar con tranferencia(15% de descuento) eliga 2"));
